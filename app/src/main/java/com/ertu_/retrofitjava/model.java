2e267a1506c80ec3c305d9f2a7a47f8cbe013ad9
Ertuğrul Şengül <ertugrul.sengul00@gmail.com>
2023-03-05 16:40:40 +0300

    model created

---
+++ app/src/main/java/com/ertu_/retrofitjava/model.java
@@ -0,0 +1,13 @@
+package com.ertu_.retrofitjava;
+
+import com.google.gson.annotations.SerializedName;
+
+public class model {
+
+    @SerializedName("currency")
+    String currency;
+
+    @SerializedName("price")
+    String price;
+
+}
